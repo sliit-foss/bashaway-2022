342f0a59538074dc8d0e905304b24ee0b702943a
Thulina Wickramasinghe <it20654580@my.sliit.lk>
2022-07-11 00:08:47 +0530

    Rules and regulations component's files added

---
+++ src/components/rulesNRegulations/rules.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rules = () => {
+    return (
+        <div class="bg-black w-full grid grid-cols-12 gap-1">
+        <div className="col-span-12"><h1 class="self-center text-white text-center text-4xl font-bold">Rules & Regulations</h1></div>
+           
+        </div>
+    );
+}
+
+export default Rules;
