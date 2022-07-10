5f4447519fc2421c182b228d23436481c6636f8d
Thulina Wickramasinghe <it20654580@my.sliit.lk>
2022-07-11 00:36:27 +0530

    Added image for rules n regulations

--- src/components/rulesNRegulations/rules.jsx
+++ src/components/rulesNRegulations/rules.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 const Rules = () => {
-    return (
-        <div class="bg-black w-full grid grid-cols-12 gap-1">
-        <div className="col-span-12"><h1 class="self-center text-white text-center text-4xl font-bold">Rules & Regulations</h1></div>
-           
-        </div>
-    );
-}
+  return (
+    <div class="bg-black w-full grid grid-cols-12 gap-1">
+      <div className="col-span-12">
+        <h1 class="self-center text-white text-center text-4xl font-bold">
+          Rules & Regulations
+        </h1>
+      </div>
+      <div className="col-span-6">
+        <img src={"/RulesNRegulations/RulesRegImg.jpg"} alt="" />
+      </div>
+      <div className="col-span-6"></div>
+    </div>
+  );
+};
 
 export default Rules;
