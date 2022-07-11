1702a08a20947c52bbecfdfc0a3e46ab539e0eca
Akalanka Perera <akalankaperera128@gmail.com>
2022-07-12 01:00:48 +0530

    Added aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 import { Header, Footer, Layout } from './components/common'
 import { Register, Sponsors, Speakers, Prizes , Timeline} from './components'
 
 function App() {
+
+  useEffect(() => {
+    Aos.init({ offset: 0, duration: 1500 })
+    window.addEventListener('load', Aos.refresh)
+  }, [])
+
   return (
     <Layout>
       <Header/>
--- src/components/speakers.jsx
+++ src/components/speakers.jsx
@@ -15,8 +15,8 @@ const Speakers = () => {
         }
     ]
 
-    const Speaker = ({ name, position, image }) => {
-        return (<div className="flex flex-col justify-center items-center my-12">
+    const Speaker = ({ name, position, image, aos }) => {
+        return (<div className="flex flex-col justify-center items-center my-12" data-aos={aos}>
             <div className={`relative w-32 h-32 md:w-44 md:h-44 mx-12 p-2 bg-black outline outline-2 outline-nav-links-unselected ring-4 rounded-full flex justify-center cursor-pointer items-center transform hover:scale-105 hover:outline-primary transition duration-300`}>
                 <img src={`/assets/speakers/${image}`} className="w-full h-full rounded-full bg-cover grayscale" />
             </div>
@@ -27,11 +27,11 @@ const Speakers = () => {
 
     return <>
         <div className="flex flex-col justify-center items-center bg-black p-4 md:p-8">
-            <Title title="Speakers" className="mt-4 mb-20" />
+            <Title title="Speakers" className="mt-4 mb-20" aos='fade-down' />
             <div className="flex flex-wrap justify-center items-center">
                 {
-                    speakerList.map((speaker) => {
-                        return <Speaker key={speaker.name} name={speaker.name} position={speaker.position} image={speaker.image} />
+                    speakerList.map((speaker, index) => {
+                        return <Speaker key={speaker.name} name={speaker.name} position={speaker.position} image={speaker.image} aos={index === 0 ? 'fade-right' : 'fade-left'} />
                     })
                 }
             </div>
