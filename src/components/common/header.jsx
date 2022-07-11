6d773c8def577108ad5537e161649791ad9a36c1
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-07-12 00:44:05 +0530

    Added burgerNavController

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,6 +1,17 @@
+import { TiThMenu } from "react-icons/ti";
+import React, { useEffect, useState } from "react";
 import { GiHamburgerMenu } from 'react-icons/gi'
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+// AOS.init();
 
 const Header = () => {
+    const [show, setShow] = useState(false);
+    const [burgerNav, setBurgerNav] = useState(false);
+
+    function burgerNavController() {
+        setBurgerNav(!burgerNav);
+    }
 
     const navItems = [
         {
@@ -36,6 +47,28 @@ const Header = () => {
     return (
         <div>
             <div className="flex flex-row md:flex-row justify-between bg-black p-2 md:p-2">
+
+                <div>
+                    <nav className='flex items-center z-50'>
+                        <TiThMenu
+                            data-aos="fade-down"
+                            data-aos-duration="1000"
+                            data-aos-offset="100"
+                            className="absolute h-8 w-8 text-[#ffffff] right-3 md:hidden mt-5"
+                            onClick={burgerNavController}
+                        />
+
+                        <ul
+                            data-aos="fade-down"
+                            data-aos-duration="1000"
+                            data-aos-offset="100"
+                            className="hidden md:flex "
+                        >
+                        </ul>
+
+                    </nav>
+                </div>
+
                 <div className="w-full md:w-5/12 pl-0 md:pl-12 md:pt-3">
                     <img src='/assets/bashaway-logo.svg' className="w-30 h-30" />
                 </div>
