914f7cc53ad565ce563b107e2f0a46a5396427d0
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-07-12 04:38:44 +0530

    partially completed header for lg size

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -63,36 +63,45 @@ const Header = () => {
     return (
         <div>
             <div className="flex flex-col md:flex-row justify-between bg-black p-2 md:p-2">
-
                 <div>
-                    <nav className='flex items-center z-50'>
+                    <nav className='flex items-center z-80 px-3 py-3'>
                         <TiThMenu
                             data-aos="fade-down"
                             data-aos-duration="1000"
                             data-aos-offset="100"
-                            className="absolute h-8 w-8 text-[#ffffff] right-3 md:hidden mt-3"
+                            className="absolute h-8 w-8 text-[#ffffff] right-3 lg:hidden mt-3 mr-3"
                             onClick={burgerNavController}
                         />
 
                         <ul
                             className={
                                 burgerNav
-                                    ? "navbar-nav mr-auto flex-col flex items-center fixed inset-0 bottom-1/2 uppercase bg-[#58dfc3]/10 backdrop-blur-lg gap-4 justify-center p-8 md:hidden  "
+                                    ? "navbar-nav mr-auto flex-col flex items-center fixed inset-0 bottom-16 uppercase bg-black backdrop-blur-3xl gap-4 justify-center p-8 lg:hidden z-50 "
                                     : "hidden"
                             }
                         >
-                            <AiOutlineClose
+                            {/* <AiOutlineClose
                                 onClick={burgerNavController}
-                                className=" h-8 w-8 text-[#ffffff] font-extrabold z-30"
-                            />
+                                className=" h-8 w-8 text-[#ffffff] font-extrabold z-30 lg:mr-48"
+                            /> */}
                             <li>
-                                <div className="w-full md:w-5/12 pl-0 md:pl-12 md:pt-3">
-                                    <img src='/assets/bashaway-logo.svg' className="w-30 h-30" />
+                                <TiThMenu
+                                    data-aos="fade-down"
+                                    data-aos-duration="1000"
+                                    data-aos-offset="100"
+                                    className=" row row-span-20absolute h-8 w-8 text-[#ffffff] right-3 lg:hidden mt-3 mr-3 md:ml-[57rem] "
+                                    onClick={burgerNavController}
+                                />
+                            </li>
+                            <li>
+                                
+                                <div className="w-full">
+                                    <img src='/assets/bashaway-logo.svg' className="w-40 h-40" />
                                 </div>
                                 {
                                     navItems.map((item) => {
-                                        return <div className="col col-span-20 md:pt-3">
-                                            <a className="text-nav-links-unselected hover:text-primary mb-3 transition duration-300" href="" target="_blank">{item.name}</a>
+                                        return <div className="col col-span-20 md:pb-10">
+                                            <a className="text-white hover:text-primary mb-3 md:py-10 transition duration-300" href="" target="_blank">{item.name}</a>
                                         </div>
                                     })
                                 }
@@ -102,25 +111,25 @@ const Header = () => {
                     </nav>
                 </div>
 
-                {/* <div className="hidden md:flex "> */}
+                <div className="hidden lg:flex justify-between w-full">
                     <div className=" w-full md:w-5/12 pl-0 md:pl-12 md:pt-3">
                         <img src='/assets/bashaway-logo.svg' className="hidden md:flex w-30 h-30" />
                     </div>
                     {
                         navItems.map((item) => {
                             return <div className="col col-span-20 md:pt-3">
-                                <a className="hidden md:flex text-nav-links-unselected hover:text-primary mb-3 transition duration-300" href="" target="_blank">{item.name}</a>
+                                <a className="hidden md:flex px-2 text-nav-links-unselected hover:text-primary mb-3 transition duration-300" href="" target="_blank">{item.name}</a>
                             </div>
                         })
                     }
-                    <div className="col col-span-20 md:pt-2 pr-10 ">
-                        <button class="hidden md:flex py-1 px-4 bg-white hover:text-primary mb-3 transition duration-300 font-semibold rounded-lg" href="" target="_blank">
+                    <div className="col col-span-20 md:pt-2 pr-7 pl-2">
+                        <button class="hidden md:flex py-1 px-3 bg-white hover:text-primary mb-3 transition duration-300 font-semibold rounded-lg" href="" target="_blank">
                             Register
                         </button>
                     </div>
                 </div>
-                <div className="hidden md:flex w-full h-[0.25px] bg bg-nav-links-unselected opacity-20"></div>
-            {/* </div> */}
+            </div>
+            <div className="hidden lg:flex w-full h-[0.25px] bg bg-nav-links-unselected opacity-20"></div>
         </div>
     )
 }
